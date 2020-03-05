0bc74d458d1f08954dc074fad152eb032f6494c7
unknown <glimsol@gmail.com>
2020-03-05 15:09:17 +0800

    edit index

--- index.js
+++ index.js
@@ -14,36 +14,38 @@ restService.use(
 restService.use(bodyParser.json());
 
 restService.post("/echo", function(req, res) {
-  var speech =
-    req.body.queryResult &&
-    req.body.queryResult.parameters &&
-    req.body.queryResult.parameters.echoText
-      ? req.body.queryResult.parameters.echoText
-      : "Seems like some problem. Speak again.";
   
-  var speechResponse = {
-    google: {
-      expectUserResponse: true,
-      richResponse: {
-        items: [
-          {
-            simpleResponse: {
-              textToSpeech: speech
-            }
-          }
-        ]
-      }
-    }
-  };
+  // var speech =
+  //   req.body.queryResult &&
+  //   req.body.queryResult.parameters &&
+  //   req.body.queryResult.parameters.echoText
+  //     ? req.body.queryResult.parameters.echoText
+  //     : "Seems like some problem. Speak again.";
   
-  return res.json({
-    payload: speechResponse,
-    //data: speechResponse,
-    fulfillmentText: speech,
-    speech: speech,
-    displayText: speech,
-    source: "webhook-echo-sample"
-  });
+  // var speechResponse = {
+  //   google: {
+  //     expectUserResponse: true,
+  //     richResponse: {
+  //       items: [
+  //         {
+  //           simpleResponse: {
+  //             textToSpeech: speech
+  //           }
+  //         }
+  //       ]
+  //     }
+  //   }
+  // };
+  
+  // return res.json({
+  //   payload: speechResponse,
+  //   //data: speechResponse,
+  //   fulfillmentText: speech,
+  //   speech: speech,
+  //   displayText: speech,
+  //   source: "webhook-echo-sample"
+  // });
+
 });
 
 restService.post("/audio", function(req, res) {
