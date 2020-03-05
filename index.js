22471e9f465a551984383fe09d7c1bcfb6d15d04
unknown <glimsol@gmail.com>
2020-03-05 15:17:21 +0800

    edit index

--- index.js
+++ index.js
@@ -15,36 +15,36 @@ restService.use(bodyParser.json());
 
 restService.post("/echo", function(req, res) {
   
-  // var speech =
-  //   req.body.queryResult &&
-  //   req.body.queryResult.parameters &&
-  //   req.body.queryResult.parameters.echoText
-  //     ? req.body.queryResult.parameters.echoText
-  //     : "Seems like some problem. Speak again.";
+  var speech =
+    req.body.queryResult &&
+    req.body.queryResult.parameters &&
+    req.body.queryResult.parameters.echoText
+      ? req.body.queryResult.parameters.echoText
+      : "Seems like some problem. Speak again.";
   
-  // var speechResponse = {
-  //   google: {
-  //     expectUserResponse: true,
-  //     richResponse: {
-  //       items: [
-  //         {
-  //           simpleResponse: {
-  //             textToSpeech: speech
-  //           }
-  //         }
-  //       ]
-  //     }
-  //   }
-  // };
+  var speechResponse = {
+    google: {
+      expectUserResponse: true,
+      richResponse: {
+        items: [
+          {
+            simpleResponse: {
+              textToSpeech: speech
+            }
+          }
+        ]
+      }
+    }
+  };
   
-  // return res.json({
-  //   payload: speechResponse,
-  //   //data: speechResponse,
-  //   fulfillmentText: speech,
-  //   speech: speech,
-  //   displayText: speech,
-  //   source: "webhook-echo-sample"
-  // });
+  return res.json({
+    payload: speechResponse,
+    //data: speechResponse,
+    fulfillmentText: speech,
+    speech: speech,
+    displayText: speech,
+    source: "webhook-echo-sample"
+  });
 
 });
 
